b70df9ec2f0f98ecb0b11ec2a6593267aa754f68
Herbrand Hofker <herbrandhofker@users.noreply.github.com>
2020-10-23 12:13:50 +0200

    generic added

---
+++ src/main/java/academy/kafka/serializers/AvroGenericDeserializer.java
@@ -0,0 +1,56 @@
+package academy.kafka.serializers;
+
+import org.apache.kafka.common.serialization.Deserializer;
+
+import java.util.Map;
+
+import java.io.IOException;
+import java.util.Iterator;
+import org.apache.avro.Schema;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.SeekableByteArrayInput;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.reflect.ReflectDatumReader;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.common.errors.SerializationException;
+
+public class AvroGenericDeserializer implements Deserializer<GenericRecord> {
+
+    private Boolean isKey;
+    private Schema schema;
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+        this.isKey = isKey;
+        this.schema = (Schema) configs.get("schema");
+    }
+
+    @Override
+    public GenericRecord deserialize(String topic, byte[] data) {
+        if (data == null) {
+            return null;
+        }
+        if (schema == null) {
+            throw new SerializationException("deserializing error: configuration expects that \"schema\" is set");
+        }
+        return readSpecificDataOneRecord(data, schema);
+    }
+
+    private GenericRecord readSpecificDataOneRecord(byte[] bytes, Schema schema) {
+       GenericRecord result = null;
+        ReflectDatumReader<GenericRecord> datumReader = new ReflectDatumReader<GenericRecord>(schema);
+        SeekableByteArrayInput inputStream = new SeekableByteArrayInput(bytes);
+        try {
+            DataFileReader<GenericRecord> dataFileReader = new DataFileReader<GenericRecord>(inputStream, datumReader);
+
+            Iterator<GenericRecord> it = dataFileReader.iterator();
+            if (it.hasNext()) {
+                result = it.next();
+            }
+            dataFileReader.close();
+        } catch (IOException ex) {
+            throw new SerializationException("Error when deserializing to  SpecificRecordBase for schema " + schema.getName());
+        }
+        return result;
+    }
+}
---
+++ src/main/java/academy/kafka/serializers/examples/AvroGenericProducer.java
@@ -0,0 +1,43 @@
+package academy.kafka.serializers.examples;
+
+import academy.kafka.serializers.AvroGenericSerializer;
+import academy.kafka.serializers.AvroSerializer;
+import java.util.Properties;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.KafkaProducer;
+
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public final class AvroGenericProducer {
+
+    public static void main(final String[] args) {
+        Properties props = new Properties();
+        props.put("schema", AvroPerson.getClassSchema());
+        props = ProducerConfig.addSerializerToConfig(props, new StringSerializer(), new AvroGenericSerializer());
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+
+        GenericRecord person = new GenericData.Record(AvroPerson.getClassSchema());
+        person.put("bsn", "BSN");
+        person.put("firstName", "Pietje");
+        person.put("lastName", "Puk");
+        person.put("bancAccount", "123");
+
+    
+        final Producer<String, GenericRecord> producer = new KafkaProducer<>(props);
+
+        for (int i = 0; i < 10; i++) {
+            final ProducerRecord<String, GenericRecord> rec = new ProducerRecord<String, GenericRecord>("avro_generic_persons",
+                    Integer.toString(i), person);
+            producer.send(rec);
+            System.out.println("produced " + i + "  " + rec.toString());
+        }
+
+        producer.close();
+    }
+
+}
